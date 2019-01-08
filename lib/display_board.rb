d1bac0ad77492b7d0e3372df9915334952d6fd0a
Charlotte Pearce <charlotte.pearce1984@gmail.com>
2019-01-08 14:42:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,11 @@
 # Define display_board that accepts a board and prints
-# out the current state.
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+
+puts 'display board'
+#board output
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
